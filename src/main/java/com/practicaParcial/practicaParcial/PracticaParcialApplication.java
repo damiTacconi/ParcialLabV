c4afc84fb9847a8609a87a790e667546e86c7672
Damian Tacconi <dami_tano_95@hotmail.com>
2019-06-24 22:52:18 -0300

    agregado EnableAsync en app

--- src/main/java/com/practicaParcial/practicaParcial/PracticaParcialApplication.java
+++ src/main/java/com/practicaParcial/practicaParcial/PracticaParcialApplication.java
@@ -2,8 +2,10 @@ package com.practicaParcial.practicaParcial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PracticaParcialApplication {
 
 	public static void main(String[] args) {
